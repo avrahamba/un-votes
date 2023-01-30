dccaa5f6cec11066f856700b8a66ae9be90f2ea0
Avraham <b.a.avraham@gmail.com>
2023-01-30 08:42:04 +0200

    change base in viteconfig

--- votes-front/vite.config.js
+++ votes-front/vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:'/un-votes/',
   build: {
-    base:'/un-votes/',
     outDir: '../dist'
   }
 })
